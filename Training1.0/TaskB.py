9b21ab50fcec87c82941c7af9a8a85cab8d955bd
IrinaZurina <zurinai@yandex.ru>
2023-02-02 12:34:41 +0300

    TaskB corrected

--- Training1.0/TaskB.py
+++ Training1.0/TaskB.py
@@ -1,7 +1,5 @@
-side1, side2, side3 = list(map(int, input().split()))
-if side1 < side2 + side3 and\
-    side2 < side1 + side3 and\
-    side3 < side1 + side2:
+side1, side2, side3 = [int(input()) for _ in range(3)]
+if side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2:
     print('YES')
 else:
     print('NO')
